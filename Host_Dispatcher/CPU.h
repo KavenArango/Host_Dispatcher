f2189e8238e5489be89e39a0ae10ff692693e68d
Kaven arango <43826797+KavenArango@users.noreply.github.com>
2020-11-11 20:57:55 -0500

    begining cpu.h file needs to be completed

--- Host_Dispatcher/CPU.h
+++ Host_Dispatcher/CPU.h
@@ -1,7 +1,16 @@
 #pragma once
+
+
+#include "Process.h"
 class CPU
 {
-
-
+public:
+	CPU();
+	void SetProcess();
+	int GetCurrentProcessPriority();
+	Process* InterruptCurrentProcess(Process*);
+private:
+	void allocateResources();
+	void releaseResources();
 };
 
