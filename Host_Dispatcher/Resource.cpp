b864adcfdd227f2ca7c033379ba00ee23206b94a
Kaven arango <43826797+KavenArango@users.noreply.github.com>
2020-11-16 18:40:19 -0500

    made the get sets and resource now stores the process id thats using it

--- Host_Dispatcher/Resource.cpp
+++ Host_Dispatcher/Resource.cpp
@@ -1,1 +1,41 @@
 #include "Resource.h"
+
+Resource::Resource()
+{
+}
+
+Resource::Resource(string newResourceName)
+{
+	resourceName = newResourceName;
+}
+
+string Resource::GetResourceName()
+{
+	return resourceName;
+}
+
+void Resource::SetResourceName(string newResourceName)
+{
+	resourceName = newResourceName;
+}
+
+bool Resource::GetInUse()
+{
+	return InUse;
+}
+
+void Resource::SetInUse(bool newInUse)
+{
+	InUse = newInUse;
+}
+
+
+int Resource::GetProcessID()
+{
+	return processID;
+}
+
+void Resource::SetProcessID(int ID)
+{
+	processID = ID;
+}
--- Host_Dispatcher/Resource.h
+++ Host_Dispatcher/Resource.h
@@ -1,15 +1,28 @@
 #pragma once
 #include <string>
 
+using namespace std;
+
 
 class Resource
 {
 public:
+	Resource();
+	Resource(string);
 
+	string GetResourceName();
+	void SetResourceName(string);
+
+	bool GetInUse();
+	void SetInUse(bool);
+
+	int GetProcessID();
+	void SetProcessID(int);
 
 private:
+	int processID;
 	string resourceName;
-
+	bool InUse = false;
 
 };
 
